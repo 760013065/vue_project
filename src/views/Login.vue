e961c30788a242b9902307d6b176d67f90927f7c
liuwenshuai <760013065@qq.com>
2022-12-13 21:30:15 +0800

    login pages

---
+++ src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <el-form :rules="rules" :model='loginForm' class="loginContainer">
+      <h3 class="loginTitle">系统登录</h3>
+      <el-form-item prop="username">
+        <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+
+      <el-form-item prop="password">
+        <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+      </el-form-item>
+<!--      <el-checkbox v-model="checked"></el-checkbox>-->
+      <el-button type="primary" style="width:  100%;">登录</el-button>
+
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+    return{
+      loginForm:{
+        username: 'admin',
+        password: '123'
+      },
+      checked:true,
+      rules:{
+        username:[{required:true,message:'请输入用户名',trigger:'blur'}],
+        password:[{required:true,message:'请输入密码',trigger:'blur'}],
+      }
+    }
+  }
+}
+
+</script>
+
+<style >
+.loginContainer{
+  border-radius: 16px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  width: 350px;
+  padding: 35px;
+  padding: 15px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #e8e0e0;
+  box-shadow: 0 0 25px #cac6c6;
+}
+.loginTitle{
+   text-align: center;
+}
+
+</style>
