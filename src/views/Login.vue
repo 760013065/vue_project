9f8fcc9f0f7aa3ff4ea9166579b28d446fb345b4
liuwenshuai <760013065@qq.com>
2022-12-13 21:47:34 +0800

    handle login events

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-form :rules="rules" :model='loginForm' class="loginContainer">
+    <el-form :rules="rules" ref="loginForm" :model='loginForm' class="loginContainer">
       <h3 class="loginTitle">系统登录</h3>
       <el-form-item prop="username">
         <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
@@ -10,7 +10,7 @@
         <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
       </el-form-item>
 <!--      <el-checkbox v-model="checked"></el-checkbox>-->
-      <el-button type="primary" style="width:  100%;">登录</el-button>
+      <el-button type="primary" style="width:  100%;" @click="submitLogin">登录</el-button>
 
     </el-form>
   </div>
@@ -31,6 +31,19 @@ export default {
         password:[{required:true,message:'请输入密码',trigger:'blur'}],
       }
     }
+  },
+  methods: {
+    submitLogin() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          this.$message.error('请输入所有字段')
+          return false;
+        }
+      });
+    }
+
   }
 }
 
